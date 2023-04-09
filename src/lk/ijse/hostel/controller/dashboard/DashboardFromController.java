df8ebfbe02ed8427fd5161d59777a9d6b47e7ce4
sudeera-madushan <sudeeramadushan888@gmail.com>
2023-04-09 22:41:55 +0530

    DashboardFrom.fxml update

--- src/lk/ijse/hostel/controller/dashboard/DashboardFromController.java
+++ src/lk/ijse/hostel/controller/dashboard/DashboardFromController.java
@@ -1,48 +1,78 @@
 package lk.ijse.hostel.controller.dashboard;
 
-import javafx.scene.control.Label;
+import javafx.application.Platform;
+import javafx.scene.Node;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 import lk.ijse.hostel.controller.util.UiNavigateUtil;
 
 public class DashboardFromController {
-    public Label lblProfile,lblStudent,lblRoom,lblHome;
+    public AnchorPane paneProfile, paneStudent, paneRoom, paneHome,paneReservation;
     public AnchorPane rootContext;
     public AnchorPane workingContext;
-    public Label lblReservation;
 
     public void lblOpenHomeOnAction(MouseEvent mouseEvent) {
-        lblProfile.setStyle("-fx-text-fill: #5153f5");
-        lblStudent.setStyle("-fx-text-fill: #5153f5");
-        lblRoom.setStyle("-fx-text-fill: #5153f5");
-        lblHome.setStyle("-fx-text-fill: white");
+        paneProfile.setStyle("-fx-background-color: none");
+        paneStudent.setStyle("-fx-background-color: none");
+        paneRoom.setStyle("-fx-background-color: none");
+        paneReservation.setStyle("-fx-background-color: none");
+        paneHome.setStyle("-fx-background-color: #adb5bd");
     }
 
     public void lblOpenRoomsOnAction(MouseEvent mouseEvent) {
-        lblProfile.setStyle("-fx-text-fill: #5153f5");
-        lblStudent.setStyle("-fx-text-fill: #5153f5");
-        lblHome.setStyle("-fx-text-fill: #5153f5");
-        lblRoom.setStyle("-fx-text-fill: white");
+        paneProfile.setStyle("-fx-background-color: none");
+        paneStudent.setStyle("-fx-background-color: none");
+        paneHome.setStyle("-fx-background-color: none");
+        paneReservation.setStyle("-fx-background-color: none");
+        paneRoom.setStyle("-fx-background-color: #adb5bd");
         UiNavigateUtil.navigationForm(workingContext,"dashboard/RoomForm");
     }
 
     public void lblOpenStudentOnAction(MouseEvent mouseEvent) {
-        lblProfile.setStyle("-fx-text-fill: #5153f5");
-        lblRoom.setStyle("-fx-text-fill: #5153f5");
-        lblHome.setStyle("-fx-text-fill: #5153f5");
-        lblStudent.setStyle("-fx-text-fill: white");
+        paneProfile.setStyle("-fx-background-color: none");
+        paneRoom.setStyle("-fx-background-color: none");
+        paneHome.setStyle("-fx-background-color: none");
+        paneReservation.setStyle("-fx-background-color: none");
+        paneStudent.setStyle("-fx-background-color: #adb5bd");
         UiNavigateUtil.navigationForm(workingContext,"dashboard/StudentForm");
     }
 
     public void lblOpenProfileOnAction(MouseEvent mouseEvent) {
-        lblStudent.setStyle("-fx-text-fill: #5153f5");
-        lblRoom.setStyle("-fx-text-fill: #5153f5");
-        lblHome.setStyle("-fx-text-fill: #5153f5");
-        lblProfile.setStyle("-fx-text-fill: white");
+        paneStudent.setStyle("-fx-background-color: none");
+        paneRoom.setStyle("-fx-background-color: none");
+        paneHome.setStyle("-fx-background-color: none");
+        paneReservation.setStyle("-fx-background-color: none");
+        paneProfile.setStyle("-fx-background-color: #adb5bd");
     }
 
     public void lblOpenReservationOnAction(MouseEvent mouseEvent) {
-
+        paneStudent.setStyle("-fx-background-color: none");
+        paneRoom.setStyle("-fx-background-color: none");
+        paneHome.setStyle("-fx-background-color: none");
+        paneReservation.setStyle("-fx-background-color: #adb5bd");
+        paneProfile.setStyle("-fx-background-color: none");
         UiNavigateUtil.navigationForm(workingContext,"dashboard/ReservationForm");
+    }
+
+    public void minimizeOnAction(MouseEvent mouseEvent) {
+        Stage window = (Stage) ((Node) mouseEvent.getSource()).getScene().getWindow();
+        window.setIconified(true);
+    }
+
+    static boolean isRestorDown=true;
+    public void maximizeOnAction(MouseEvent mouseEvent) {
+        Stage window = (Stage) ((Node) mouseEvent.getSource()).getScene().getWindow();
+        if (isRestorDown) {
+            window.setFullScreen(true);
+            isRestorDown=false;
+        }else {
+            window.setFullScreen(false);
+            isRestorDown=true;
+        }
+    }
+
+    public void closeOnAction(MouseEvent mouseEvent) {
+        Platform.exit();
     }
 }
